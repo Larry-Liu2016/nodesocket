ba0423168dbe7d2cc285f12d98cdffd8bf442f9b
Wenchao Liu <wenchaoliu@Wenchaos-MacBook-Pro.local>
2017-02-21 13:13:26 +1100

    yeah

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '/../public');
-
+const socketIO = require ("socket.io");
 const express = require('express');
 const port = process.env.PORT || 3000;
+
 var app = express();
+var server = app.listen(port);
+var io = socketIO.listen(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connect');
+    socket.on('disconnect', () => {
+        console.log('Disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
